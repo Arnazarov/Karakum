366f847dbd35722198690894aa271da2e00970a0
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-04-27 01:14:41 -0500

    convert commonjs syntax to es6

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -6,7 +6,7 @@ import Rating from './Rating';
 const Product = ({ product }) => (
   <Card className="my-3 p-3 rounded">
     <Link to={`/product/${product._id}`}>
-      <Card.Img src={product.image} width="500" height="350" variant="top" />
+      <Card.Img src={product.image} variant="top" />
     </Link>
 
     <Card.Body>
