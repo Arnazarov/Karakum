19f8831437e52de309d195b5c4b2c314cd7817dd
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-04-27 18:40:10 -0500

    added product routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,36 @@
+import express from "express";
+import Product from '../models-schemas/productModel.js';
+
+const router = express.Router();
+
+// @desc    Get all products
+// @route   GET /api/products
+// @access  Public
+router.get('/', (req, res) => {
+
+    Product.find((err, products) => {
+        if (products) {
+            res.send(products);
+        } else {
+            res.status(404).json({ message: 'Products not found' });
+            if (err) console.error(err);
+        }
+    })
+});
+
+// @desc    Get single product
+// @route   GET /api/products/:id
+// @access  Public
+router.get('/:id', (req, res) => {
+
+    Product.findById(req.params.id, (err, product) => {
+        if (product) {
+            res.send(product);
+        } else {
+            res.status(404).json({ message: 'Product not found' });
+            if (err) console.error(err);
+        }
+    })
+});
+
+export default router;
