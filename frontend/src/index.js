840ced82c8acf3016d403c3ef4284fd991211c01
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-04-28 01:18:14 -0500

    redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './bootstrap2.min.css';
 import './index.css';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import store from './storeRedux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>{' '}
+  </Provider>
 );
-
