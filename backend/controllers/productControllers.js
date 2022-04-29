7970024023547c12034d515c0cbe242a2cad282f
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-04-28 22:47:27 -0500

    added controller for products at backend

---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,29 @@
+import Product from '../models-schemas/productModel.js';
+
+// @desc    Get all products
+// @route   GET /api/products
+// @access  Public
+export const getAllProducts = (req, res) => {
+
+    Product.find((err, products) => {
+        if (products) {
+            res.send(products);
+        } else {
+            res.status(404).json({ message: 'Products not found', stack: err.stack });
+        }
+    })
+}
+
+// @desc    Get single product
+// @route   GET /api/products/:id
+// @access  Public
+export const getProductById = (req, res) => {
+
+    Product.findById(req.params.id, (err, product) => {
+        if (product) {
+            res.send(product);
+        } else {
+            res.status(404).json({ message: 'Product not found', stack: err.stack });
+        }
+    })
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,9 @@
 import express from "express";
-import Product from '../models-schemas/productModel.js';
-
+import { getAllProducts, getProductById } from '../controllers/productControllers.js'
+ 
 const router = express.Router();
 
-// @desc    Get all products
-// @route   GET /api/products
-// @access  Public
-router.get('/', (req, res) => {
-
-    Product.find((err, products) => {
-        if (products) {
-            res.send(products);
-        } else {
-            res.status(404).json({ message: 'Products not found', stack: err.stack });
-        }
-    })
-});
-
-// @desc    Get single product
-// @route   GET /api/products/:id
-// @access  Public
-router.get('/:id', (req, res) => {
-
-    Product.findById(req.params.id, (err, product) => {
-        if (product) {
-            res.send(product);
-        } else {
-            res.status(404).json({ message: 'Product not found', stack: err.stack });
-        }
-    })
-});
+router.get('/', getAllProducts)
+router.get('/:id', getProductById)
 
 export default router;
