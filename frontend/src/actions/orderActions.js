209d7bb3652d437904b965b66316526d82370054
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-05-03 01:02:02 -0500

    added getUserOrders action/reducer at frontend

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,4 +1,4 @@
-import {ORDER_CREATE_REQUEST, ORDER_CREATE_FAIL, ORDER_CREATE_SUCCESS, ORDER_GET_REQUEST, ORDER_GET_SUCCESS, ORDER_GET_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL} from '../constants/orderConstants'
+import {ORDER_CREATE_REQUEST, ORDER_CREATE_FAIL, ORDER_CREATE_SUCCESS, ORDER_GET_REQUEST, ORDER_GET_SUCCESS, ORDER_GET_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL, ORDER_USER_REQUEST, ORDER_USER_SUCCESS, ORDER_USER_FAIL} from '../constants/orderConstants'
 import axios from "axios"
 
 export const createOrderAction = (order) => async (dispatch, getState) => {
@@ -89,6 +89,37 @@ export const updateOrderWithPayAction = (orderID, paymentSummary) => async (disp
     } catch(err) {
         dispatch({
             type: ORDER_PAY_FAIL,
+            payload: err.response && err.response.data.message ? err.response.data.message : err.message
+        })
+    }
+
+}
+
+export const getUserOrdersAction = () => async (dispatch, getState) => {
+
+    try {
+
+        dispatch({type: ORDER_USER_REQUEST})
+
+        const { userLogin: {userLoginInfo }} = getState();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userLoginInfo.token}`
+            }
+        }
+
+        const { data } = await axios.get(`/api/orders/myorders`, config)
+
+        dispatch({
+            type: ORDER_USER_SUCCESS,
+            payload: data
+        })
+
+
+    } catch(err) {
+        dispatch({
+            type: ORDER_USER_FAIL,
             payload: err.response && err.response.data.message ? err.response.data.message : err.message
         })
     }
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,4 +1,4 @@
-import {ORDER_CREATE_REQUEST, ORDER_CREATE_FAIL, ORDER_CREATE_SUCCESS, ORDER_GET_REQUEST, ORDER_GET_SUCCESS, ORDER_GET_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_RESET} from '../constants/orderConstants'
+import {ORDER_CREATE_REQUEST, ORDER_CREATE_FAIL, ORDER_CREATE_SUCCESS, ORDER_GET_REQUEST, ORDER_GET_SUCCESS, ORDER_GET_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_RESET, ORDER_USER_REQUEST, ORDER_USER_SUCCESS, ORDER_USER_FAIL} from '../constants/orderConstants'
 
 export const createOrderReducer = (state = {}, action) => {
     switch(action.type) {
@@ -49,6 +49,25 @@ export const updateOrderWithPayReducer = (state = {}, action) => {
             }
         case ORDER_PAY_RESET: 
             return { }
+        default:
+            return state 
+    }
+}
+
+export const getUserOrdersReducer = (state = { order: [] }, action) => {
+    switch(action.type) {
+        case ORDER_USER_REQUEST:
+            return {
+                loading: true
+            } 
+        case ORDER_USER_SUCCESS:
+            return {
+                loading: false, orders: action.payload
+            }
+        case ORDER_USER_FAIL: 
+            return {
+                loading: false, error:action.payload 
+            }
         default:
             return state 
     }
