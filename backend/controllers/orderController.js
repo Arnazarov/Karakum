2b27694c595f60bdc56e80c1866f145d5514d631
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-05-02 20:43:18 -0500

    added getOrderById route at backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,29 +1,54 @@
 import Order from "../models-schemas/orderModel.js";
+
+
 // @desc    Create an order
 // @route   POST /api/orders
 // @access  Private
-
 export const createOrder = async(req, res, next) => {
-    const {itemsInOrder, shippingInfo, paymentInfo, paymentSummary, taxCost, shippingCost, totalCost} = req.body;
+    try {
+        const {itemsInOrder, shippingInfo, paymentInfo, paymentSummary, taxCost, shippingCost, totalCost} = req.body;
 
-    if (itemsInOrder && itemsInOrder.length === 0) {
-        res.status(400);
-        next(new Error('No order items'))
-        return;
-    } else {
-        const order = new Order({
-            user: req.user._id,
-            itemsInOrder, 
-            shippingInfo, 
-            paymentInfo, 
-            paymentSummary, 
-            taxCost, 
-            shippingCost, 
-            totalCost
-        })
+        if (itemsInOrder && itemsInOrder.length === 0) {
+            res.status(400);
+            next(new Error('No order items'))
+            return;
+        } else {
+            const order = new Order({
+                user: req.user._id,
+                itemsInOrder, 
+                shippingInfo, 
+                paymentInfo, 
+                paymentSummary, 
+                taxCost, 
+                shippingCost, 
+                totalCost
+            })
 
-        const createdOrder = await order.save();
+            const createdOrder = await order.save();
 
-        res.status(201).json(createdOrder);
+            res.status(201).json(createdOrder);
+        }
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+// @desc    Fecth an order by id
+// @route   GET /api/orders/:id
+// @access  Private
+export const getOrderById = async(req, res, next) => {
+    try {
+        
+        const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+        if (order) {
+            res.json(order)
+        } else {
+            res.status(404);
+            next(new Error('Order not found :('));
+        }
+  
+    } catch(err) {
+        console.error(err);
     }
 }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {createOrder} from '../controllers/orderController.js'
+import {createOrder, getOrderById} from '../controllers/orderController.js'
 import protectKarakum from '../middleware/authMware.js'
 const router = express.Router();
 
 router.route('/').post(protectKarakum, createOrder);
+router.route('/:id').get(protectKarakum, getOrderById);
 
 export default router;
 
