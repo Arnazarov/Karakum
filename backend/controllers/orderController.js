db0eb0df85c97d9f975a92f9b541865954559673
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-05-02 22:15:31 -0500

    added updateOrderWithPaidStatus route at backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -52,3 +52,35 @@ export const getOrderById = async(req, res, next) => {
         console.error(err);
     }
 }
+
+// @desc    Update order with paid status
+// @route   PUT /api/orders/:id/pay
+// @access  Private
+export const updateOrderWithPaidStatus = async(req, res, next) => {
+    try {
+        
+        const order = await Order.findById(req.params.id);
+
+        if (order) {
+            order.isPaid = true;
+            order.paidAt = Date.now();
+            order.paymentSummary = {
+                id: req.body.id,
+                status: req.body.status,
+                updateTime: req.body.update_time,
+                email: req.body.payer.email_address
+            }
+
+            const updatedOrder = await order.save();
+
+            res.json(updatedOrder)
+
+        } else {
+            res.status(404);
+            next(new Error('Order not found :('));
+        }
+  
+    } catch(err) {
+        console.error(err);
+    }
+}
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {createOrder, getOrderById} from '../controllers/orderController.js'
+import {createOrder, getOrderById, updateOrderWithPaidStatus} from '../controllers/orderController.js'
 import protectKarakum from '../middleware/authMware.js'
 const router = express.Router();
 
 router.route('/').post(protectKarakum, createOrder);
 router.route('/:id').get(protectKarakum, getOrderById);
+router.route('/:id/pay').put(protectKarakum, updateOrderWithPaidStatus);
 
 export default router;
 
