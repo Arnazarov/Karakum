06bb5facae2aa9d33c285ff725a3c36247117faf
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-05-04 00:30:03 -0500

    created actions/reducers for creating a single product

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+import { PRODUCT_CREATE_FAIL, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
     try {
@@ -61,6 +61,37 @@ export const deleteProductAction = (id) => async (dispatch, getState) => {
         console.log(err);
         dispatch({
             type: PRODUCT_DELETE_FAIL,
+            payload: err.response && err.response.data.message ? err.response.data.message : err.message
+        })
+    }
+}
+
+export const createProductAction = () => async (dispatch, getState) => {
+    try {
+
+        dispatch({
+            type: PRODUCT_CREATE_REQUEST
+        });
+
+        const { userLogin: {userLoginInfo}} = getState();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userLoginInfo.token}`
+            }
+        }
+
+        const {data} = await axios.post(`/api/products`, {}, config);
+
+        dispatch({
+            type: PRODUCT_CREATE_SUCCESS,
+            payload: data 
+        });
+
+    } catch(err) {
+        console.log(err);
+        dispatch({
+            type: PRODUCT_CREATE_FAIL,
             payload: err.response && err.response.data.message ? err.response.data.message : err.message
         })
     }
