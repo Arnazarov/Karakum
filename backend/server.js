a3c7986f367dd9596758600a9671a039915e8d43
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-05-07 21:52:22 -0500

    prepared for production

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,14 @@ import uploadRoutes from './routes/uploadRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMware.js'
 
 dotenv.config();
-
 const app = express()
 mongoDB();
+app.use(express.json())
+
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-
-// Body parser
-app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.send('API is running')
-});
 
 // ROUTE handling middleware
 app.use('/api/products', productRoutes);
@@ -32,9 +26,20 @@ app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes)
 
+//  PayPal payment handling middleware
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
 const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+    app.get('/', (req, res) => {
+        res.send('Development mode is running')
+    })
+}
+
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 // ERROR handling middleware
